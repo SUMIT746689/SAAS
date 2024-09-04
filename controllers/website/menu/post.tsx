2bd58e692cf55d21f266698719d1e1104988ff3b
mukta35-2733@diu.edu.bd <mukta35-2733@diu.edu.bd>
2024-09-04 10:14:43 +0600

    controller/website added in coaching-main branch

---
+++ controllers/website/menu/post.tsx
@@ -0,0 +1,46 @@
+import prisma from '@/lib/prisma_client';
+import { authenticate } from 'middleware/authenticate';
+import { logFile } from 'utilities_api/handleLogFile';
+
+async function post(req, res, refresh_token) {
+  try {
+    const { parent_id, english_title, bangla_title, link_type, website_link, dynamic_page_link_id, status } = req.body;
+    const data = {};
+
+    const singleMenuItem = await prisma.websiteMenu.findFirst({
+      where: {
+        id: parseInt(parent_id)
+      }
+    });
+
+    if (singleMenuItem) data['parent_id'] = parseInt(parent_id);
+    if (bangla_title) data['bangla_title'] = bangla_title;
+    if (!english_title) return res.status(404).send({ message: 'No english title found!' });
+    if (!link_type) return res.status(404).send({ message: 'No link type found!' });
+    if (link_type?.toLowerCase() === 'dynamic page link') {
+      if (!dynamic_page_link_id) return res.status(404).send({ message: 'No dynamic page id found!' });
+    } else {
+      if (!website_link) return res.status(404).send({ message: 'No website link found!' });
+    }
+    if (!status) return res.status(404).send({ message: 'No status found!' });
+
+    await prisma.websiteMenu.create({
+      data: {
+        ...data,
+        english_title: english_title.toLowerCase(),
+        link_type: link_type.toLowerCase(),
+        website_link: website_link,
+        status: status.toLowerCase(),
+        dynamic_page_id: dynamic_page_link_id,
+        school_id: parseInt(refresh_token.school_id)
+      }
+    });
+
+    return res.json({ message: 'website menu created successfully!' });
+  } catch (err) {
+    logFile.error(err.message);
+    res.status(404).json({ error: err.message });
+  }
+}
+
+export default authenticate(post);
