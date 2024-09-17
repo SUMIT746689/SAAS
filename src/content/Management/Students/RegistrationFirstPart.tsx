d9e37e67ac838e0e6747d81cbc574dc153e27a78
mukta35-2733@diu.edu.bd <mukta35-2733@diu.edu.bd>
2024-09-17 14:46:22 +0600

    student registration 1 2 3 4 page added in coaching main from main

--- src/content/Management/Students/RegistrationFirstPart.tsx
+++ src/content/Management/Students/RegistrationFirstPart.tsx
@@ -5,14 +5,7 @@ import { useTranslation } from 'react-i18next';
 
 import dayjs, { Dayjs } from 'dayjs';
 
-import {
-  Grid,
-  DialogActions,
-  DialogContent,
-  TextField,
-  CircularProgress,
-  Button,
-} from '@mui/material';
+import { Grid, DialogActions, DialogContent, TextField, CircularProgress, Button } from '@mui/material';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Radio from '@mui/material/Radio';
@@ -23,41 +16,33 @@ import { MobileDatePicker, MobileDateTimePicker } from '@mui/lab';
 import { registration_no_generate } from '@/utils/utilitY-functions';
 import { handleConvBanNum } from 'utilities_api/convertBanFormatNumber';
 
-function RegistrationFirstPart({
-  totalFormData,
-  setTotalFormData,
-  setActiveStep,
-  handleCreateClassClose,
-  student = null,
-}) {
+function RegistrationFirstPart({ totalFormData, setTotalFormData, setActiveStep, handleCreateClassClose, student = null }) {
   const { t }: { t: any } = useTranslation();
   const { showNotification } = useNotistick();
 
-  console.log(totalFormData)
+  console.log(totalFormData);
   return (
     <>
       <Formik
         initialValues={{
-          first_name: student ? (student?.first_name || student?.student_info?.first_name || '') : totalFormData.first_name || undefined,
-          middle_name: student ? (student?.middle_name || student?.student_info?.middle_name || '') : totalFormData.middle_name  || undefined,
-          last_name: student ? (student?.middle_name || student?.student_info?.last_name || '') : totalFormData.last_name || undefined,
-          student_id: student ? (student?.student_id || student?.student_info?.student_id) : totalFormData.last_name || undefined,
-          admission_no: student ? (student?.admission_no || student?.student_info?.admission_no) : totalFormData.admission_no || undefined,
-          admission_date: student ? (student?.admission_date || student?.student_info?.admission_date) : totalFormData.admission_date,
-          date_of_birth: student ? (student?.date_of_birth || student?.student_info?.date_of_birth) : totalFormData.date_of_birth,
-          gender: student ? (student?.gender || student?.student_info?.gender) : totalFormData.gender || undefined,
-          blood_group: student ? (student?.blood_group || student?.student_info?.blood_group || '') : totalFormData.blood_group || undefined,
-          religion: student ? (student?.religion || student?.student_info?.religion) : totalFormData.religion || undefined,
-          phone: student ? (student?.phone || student?.student_info?.phone) : totalFormData.phone || undefined,
-          email: student ? (student?.email || student?.student_info?.email) : totalFormData.email || undefined,
-          national_id: student ? (student?.national_id || student?.student_info?.national_id) : totalFormData.national_id || undefined,
-          card_no: student  ? (student?.card_no || student?.student_info?.card_no) : totalFormData.card_no || undefined,
+          first_name: student ? student?.first_name || student?.student_info?.first_name || '' : totalFormData.first_name || undefined,
+          middle_name: student ? student?.middle_name || student?.student_info?.middle_name || '' : totalFormData.middle_name || undefined,
+          last_name: student ? student?.middle_name || student?.student_info?.last_name || '' : totalFormData.last_name || undefined,
+          student_id: student ? student?.student_id || student?.student_info?.student_id : totalFormData.last_name || undefined,
+          admission_no: student ? student?.admission_no || student?.student_info?.admission_no : totalFormData.admission_no || undefined,
+          admission_date: student ? student?.admission_date || student?.student_info?.admission_date : totalFormData.admission_date,
+          date_of_birth: student ? student?.date_of_birth || student?.student_info?.date_of_birth : totalFormData.date_of_birth,
+          gender: student ? student?.gender || student?.student_info?.gender : totalFormData.gender || undefined,
+          blood_group: student ? student?.blood_group || student?.student_info?.blood_group || '' : totalFormData.blood_group || undefined,
+          religion: student ? student?.religion || student?.student_info?.religion : totalFormData.religion || undefined,
+          phone: student ? student?.phone || student?.student_info?.phone : totalFormData.phone || undefined,
+          email: student ? student?.email || student?.student_info?.email : totalFormData.email || undefined,
+          national_id: student ? student?.national_id || student?.student_info?.national_id : totalFormData.national_id || undefined,
+          card_no: student ? student?.card_no || student?.student_info?.card_no : totalFormData.card_no || undefined,
           student_photo: null
         }}
         validationSchema={Yup.object().shape({
-          first_name: Yup.string()
-            .max(255)
-            .required(t('First name field is required')),
+          first_name: Yup.string().max(255).required(t('First name field is required')),
           middle_name: Yup.string().max(255).nullable(true),
           last_name: Yup.string().max(255).nullable(true),
           admission_date: Yup.date().required(t('Admission date is required!')),
@@ -65,14 +50,9 @@ function RegistrationFirstPart({
           gender: Yup.string().required(t('select a gender')),
           blood_group: Yup.string().nullable(true),
           religion: Yup.string().nullable(true),
-          phone: Yup.string().required(t('Phone number is required!'))
-            .min(11, 'Phone number must be greater then or equals 11 character'),
-
+          phone: Yup.string().required(t('Phone number is required!')).min(11, 'Phone number must be greater then or equals 11 character')
         })}
-        onSubmit={async (
-          _values,
-          { resetForm, setErrors, setStatus, setSubmitting }
-        ) => {
+        onSubmit={async (_values, { resetForm, setErrors, setStatus, setSubmitting }) => {
           try {
             const { number, err } = handleConvBanNum(_values.phone);
             if (err) return showNotification(err, 'error');
@@ -90,16 +70,7 @@ function RegistrationFirstPart({
           }
         }}
       >
-        {({
-          errors,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-          isSubmitting,
-          touched,
-          values,
-          setFieldValue
-        }) => {
+        {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values, setFieldValue }) => {
           // console.log("T__values__",values);
           // console.log({ errors });
           return (
@@ -143,9 +114,7 @@ function RegistrationFirstPart({
                             borderRadius: '3px'
                           }
                         }}
-                        error={Boolean(
-                          touched.middle_name && errors.middle_name
-                        )}
+                        error={Boolean(touched.middle_name && errors.middle_name)}
                         fullWidth
                         helperText={touched.middle_name && errors.middle_name}
                         label={t('Middle name')}
@@ -178,7 +147,7 @@ function RegistrationFirstPart({
                     </Grid>
 
                     {/* admission_no   */}
-                    <Grid item xs={12} display={"grid"} gridTemplateColumns={{ md: "1fr 1fr" }} gap={2}>
+                    <Grid item xs={12} display={'grid'} gridTemplateColumns={{ md: '1fr 1fr' }} gap={2}>
                       <TextField
                         // required
                         size="small"
@@ -187,9 +156,7 @@ function RegistrationFirstPart({
                             borderRadius: '3px'
                           }
                         }}
-                        error={Boolean(
-                          touched.admission_no && errors.admission_no
-                        )}
+                        error={Boolean(touched.admission_no && errors.admission_no)}
                         fullWidth
                         helperText={touched.admission_no && errors.admission_no}
                         label={t('Admission no')}
@@ -208,9 +175,7 @@ function RegistrationFirstPart({
                             borderRadius: '3px'
                           }
                         }}
-                        error={Boolean(
-                          touched.student_id && errors.student_id
-                        )}
+                        error={Boolean(touched.student_id && errors.student_id)}
                         fullWidth
                         helperText={touched.student_id && errors.student_id}
                         label={t('Student Id')}
@@ -222,59 +187,55 @@ function RegistrationFirstPart({
                       />
                     </Grid>
 
-
                     {/* admission_date */}
                     <Grid item xs={12} md={6}>
                       <MobileDateTimePicker
                         label="admission Date"
-                        inputFormat='dd/MM/yyyy hh:mm a'
+                        inputFormat="dd/MM/yyyy hh:mm a"
                         value={values.admission_date}
-                        
                         renderInput={(params) => (
                           <TextField
                             required
                             fullWidth
                             size="small"
-                            name='admission_date'
+                            name="admission_date"
                             onBlur={handleBlur}
                             sx={{
                               '& fieldset': {
                                 borderRadius: '3px'
-                              },
+                              }
                             }}
                             {...params}
                           />
                         )}
                         onChange={(n: any) => {
                           if (n) {
-                            const newValue = dayjs(n)
-                            console.log("admission_date__", newValue);
+                            const newValue = dayjs(n);
+                            console.log('admission_date__', newValue);
                             //@ts-ignore
                             setFieldValue('admission_date', newValue);
                           }
                         }}
                       />
-                      {
-                        errors.admission_date && <span style={{ color: 'red' }}> Admission date are required</span>
-                      }
+                      {errors.admission_date && <span style={{ color: 'red' }}> Admission date are required</span>}
                     </Grid>
                     {/* date_of_birth */}
                     <Grid item xs={12} md={6}>
                       <MobileDatePicker
                         label="Date of birth"
-                        inputFormat='dd/MM/yyyy'
+                        inputFormat="dd/MM/yyyy"
                         value={values.date_of_birth}
                         onChange={(n) => {
                           if (n) {
                             // @ts-ignore
-                            const newValue = dayjs(n)
+                            const newValue = dayjs(n);
                             setFieldValue('date_of_birth', newValue);
                           }
                         }}
                         renderInput={(params) => (
                           <TextField
                             fullWidth
-                            name='date_of_birth'
+                            name="date_of_birth"
                             size="small"
                             sx={{
                               '& fieldset': {
@@ -285,17 +246,13 @@ function RegistrationFirstPart({
                           />
                         )}
                       />
-                      {
-                        errors.date_of_birth && <span style={{ color: 'red' }}> Date of birth are required</span>
-                      }
+                      {errors.date_of_birth && <span style={{ color: 'red' }}> Date of birth are required</span>}
                     </Grid>
 
                     {/* Gender */}
                     <Grid item xs={12} sm={6} md={6}>
                       <FormControl required>
-                        <FormLabel id="demo-row-radio-buttons-group-label">
-                          Select Gender
-                        </FormLabel>
+                        <FormLabel id="demo-row-radio-buttons-group-label">Select Gender</FormLabel>
                         <RadioGroup
                           aria-labelledby="demo-controlled-radio-buttons-group"
                           name="gender"
@@ -305,16 +262,8 @@ function RegistrationFirstPart({
                             setFieldValue('gender', event.target.value);
                           }}
                         >
-                          <FormControlLabel
-                            value="male"
-                            control={<Radio />}
-                            label="Male"
-                          />
-                          <FormControlLabel
-                            value="female"
-                            control={<Radio />}
-                            label="Female"
-                          />
+                          <FormControlLabel value="male" control={<Radio />} label="Male" />
+                          <FormControlLabel value="female" control={<Radio />} label="Female" />
                         </RadioGroup>
                       </FormControl>
                     </Grid>
@@ -327,9 +276,7 @@ function RegistrationFirstPart({
                             borderRadius: '3px'
                           }
                         }}
-                        error={Boolean(
-                          touched.card_no && errors.card_no
-                        )}
+                        error={Boolean(touched.card_no && errors.card_no)}
                         fullWidth
                         helperText={touched.card_no && errors.card_no}
                         label={t('Card Number')}
@@ -351,9 +298,7 @@ function RegistrationFirstPart({
                             borderRadius: '3px'
                           }
                         }}
-                        error={Boolean(
-                          touched.blood_group && errors.blood_group
-                        )}
+                        error={Boolean(touched.blood_group && errors.blood_group)}
                         fullWidth
                         helperText={touched.blood_group && errors.blood_group}
                         label={t('blood Group')}
@@ -435,7 +380,7 @@ function RegistrationFirstPart({
                     </Grid>
 
                     {/* national_id */}
-                    <Grid item xs={12} >
+                    <Grid item xs={12}>
                       <TextField
                         size="small"
                         sx={{
@@ -443,9 +388,7 @@ function RegistrationFirstPart({
                             borderRadius: '3px'
                           }
                         }}
-                        error={Boolean(
-                          touched.national_id && errors.national_id
-                        )}
+                        error={Boolean(touched.national_id && errors.national_id)}
                         fullWidth
                         helperText={touched.national_id && errors.national_id}
                         label={t('Birth certificate Id')}
@@ -465,25 +408,19 @@ function RegistrationFirstPart({
                   p: 3
                 }}
               >
-                
                 <Button color="secondary" onClick={handleCreateClassClose}>
                   {t('Cancel')}
                 </Button>
-                
+
                 <Button
                   type="submit"
-                  startIcon={
-                    isSubmitting ? <CircularProgress size="1rem" /> : null
-                  }
+                  startIcon={isSubmitting ? <CircularProgress size="1rem" /> : null}
                   // @ts-ignore
                   disabled={Boolean(errors.submit) || isSubmitting}
                   variant="contained"
                 >
                   {t('Next')}
                 </Button>
-
-
-
               </DialogActions>
             </form>
           );
