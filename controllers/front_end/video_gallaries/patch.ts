0e4612007f82d68c849ad7f51c744893ca7e15dc
mukta35-2733@diu.edu.bd <mukta35-2733@diu.edu.bd>
2024-09-29 12:08:14 +0600

    video gallary completely done

--- controllers/front_end/video_gallaries/patch.ts
+++ controllers/front_end/video_gallaries/patch.ts
@@ -1,36 +1,50 @@
+
 import { authenticate } from 'middleware/authenticate';
 import prisma from '@/lib/prisma_client';
 import { logFile } from 'utilities_api/handleLogFile';
+
 async function put(req, res, refresh_token) {
   try {
     const id = parseInt(req.query.id);
-    if (Number.isNaN(id)) throw new Error('Provide invalid id ');
+    if (Number.isNaN(id)) throw new Error('Provided invalid id');
+    
     const { school_id } = refresh_token;
-    const {youtube_link,english_title,bangla_title} = req.body;
-
-    const response = await prisma.websiteUi.update({
+    const { youtube_link, english_title, bangla_title } = req.body;
+    const resWebsiteUi = await prisma.websiteUi.findFirst({
       where: {
-        id,
+        school_id
+      }
+    });
+
+    if (!resWebsiteUi) {
+      throw new Error(`school_id ${school_id} not found`);
+    }
+    const updatedVedioGallery = resWebsiteUi.video_gallery?.map(vg => {
+      if (vg.id === id) {
+        return {
+          ...vg,
+          youtube_link: youtube_link || vg.youtube_link,
+          english_title: english_title || vg.english_title,
+          bangla_title: bangla_title || vg.bangla_title
+        };
+      }
+      return vg;
+    });
+    const updatedRecord = await prisma.websiteUi.updateMany({
+      where: {
         school_id
       },
       data: {
-        video_gallery: [{
-        youtube_link: youtube_link || undefined,
-        english_title: english_title || undefined,
-        bangla_title: bangla_title || undefined,
-        }]
+        video_gallery: updatedVedioGallery
       }
-    })
+    });
 
-   
-
-    res.json({ data: response, success: true });
-
+    res.json({ data: updatedRecord, success: true });
   } catch (err) {
-    logFile.error(err.message)
+    logFile.error(err.message);
     console.log({ err: err.message });
     res.status(404).json({ error: err.message });
   }
 }
 
-export default authenticate(put)
+export default authenticate(put);
