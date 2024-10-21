28755b4b0f54afe79db5eb80ebd1d72eb984a87f
mukta35-2733@diu.edu.bd <mukta35-2733@diu.edu.bd>
2024-10-21 16:45:12 +0600

    get branch from student branch

--- website/src/app/(default)/branches/page.jsx
+++ website/src/app/(default)/branches/page.jsx
@@ -1,86 +1,35 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import { Grid, Dialog, DialogContent, Button } from '@mui/material';
-import { FaPhoneAlt } from "react-icons/fa";import { IoLocationSharp } from "react-icons/io5";
-function page({classes, editData, setEditData, reFetchData , serverHost}) {
+import prisma from '@/lib/prisma_client';
+import Branches from './Branches';
+import { headers } from 'next/headers';
+
+export default async function Branch() {
+  const headersList = headers();
+  const domain = headersList.get('host');
+
+  const schoolData = await prisma.school.findFirst({
+    where: { domain },
+    select: {
+      name: true,
+      School: {
+        select: {
+          name: true,
+          phone: true,
+          optional_phone: true,
+          map_location: true,
+        },
+      },
+    },
+  });
+
+  const branches = schoolData?.School || [];
+  console.log("Branches Data:", JSON.stringify(branches, null, 2));
+
   return (
-    <> 
-   <div className='flex flex-row  my-8'>
-   <div className=' bg-slate-300 flex flex-col gap-4 mx-auto px-6 py-6 rounded-2xl'>
-      <div className='flex flex-row gap-8 justify-between ' >
-        <h3 className='font-bold text-black text-xl' >বনশ্রী</h3> 
-        <p className='text-red-500 text-4xl'>
-        <IoLocationSharp />
-        </p>
-        
-      </div> 
-      <div className='flex flex-row  gap-8 justify-between'>
-        <div className='flex flex-row gap-1'>
-          <p className='bg-green-600 rounded-full my-auto p-1 text-white'>
-          <FaPhoneAlt />
-          </p>
-          <p>01302564259</p>
-        </div>
-
-        <div className='flex flex-row gap-1'>
-          <p className='bg-green-600 rounded-full my-auto p-1 text-white'>
-          <FaPhoneAlt />
-          </p>
-          <p>01302564259</p>
-        </div>
-      </div>
+    <div>
+      <Branches
+        branches={branches}
+        serverHost={`${process.env.SERVER_HOST}` || ''}
+      />
     </div>
-    <div className=' bg-slate-300 flex flex-col gap-4 mx-auto px-6 py-6 rounded-2xl'>
-      <div className='flex flex-row gap-8 justify-between ' >
-        <h3 className='font-bold text-black text-xl' >বনশ্রী</h3> 
-        <p className='text-red-500 text-4xl'>
-        <IoLocationSharp />
-        </p>
-        
-      </div> 
-      <div className='flex flex-row  gap-8 justify-between'>
-        <div className='flex flex-row gap-1'>
-          <p className='bg-green-600 rounded-full my-auto p-1 text-white'>
-          <FaPhoneAlt />
-          </p>
-          <p>01302564259</p>
-        </div>
-
-        <div className='flex flex-row gap-1'>
-          <p className='bg-green-600 rounded-full my-auto p-1 text-white'>
-          <FaPhoneAlt />
-          </p>
-          <p>01302564259</p>
-        </div>
-      </div>
-    </div>
-    <div className=' bg-slate-300 flex flex-col gap-4 mx-auto px-6 py-6 rounded-2xl'>
-      <div className='flex flex-row gap-8 justify-between ' >
-        <h3 className='font-bold text-black text-xl' >বনশ্রী</h3> 
-        <p className='text-red-500 text-4xl'>
-        <IoLocationSharp />
-        </p>
-        
-      </div> 
-      <div className='flex flex-row  gap-8 justify-between'>
-        <div className='flex flex-row gap-1'>
-          <p className='bg-green-600 rounded-full my-auto p-1 text-white'>
-          <FaPhoneAlt />
-          </p>
-          <p>01302564259</p>
-        </div>
-
-        <div className='flex flex-row gap-1'>
-          <p className='bg-green-600 rounded-full my-auto p-1 text-white'>
-          <FaPhoneAlt />
-          </p>
-          <p>01302564259</p>
-        </div>
-      </div>
-    </div>
-    
-   </div>
-    </>
   );
 }
-export default page;
