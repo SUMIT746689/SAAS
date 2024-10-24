0bd0ef780eebaa32e9b5d77ee9cc6764742ce98e
MHMHMH.183002131mhmh <mehedi@elitbuzz-bd.com>
2024-10-24 13:25:05 +0600

    fix website ui

--- website/src/components/Branches.jsx
+++ website/src/components/Branches.jsx
@@ -1,51 +1,60 @@
 "use client";
-import React from 'react';
+import React, { useContext } from 'react';
 import { IoLocationSharp } from "react-icons/io5";
-import { FaPhoneAlt } from "react-icons/fa";import { FaExternalLinkAlt } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaExternalLinkAlt } from "react-icons/fa";
 import Link from 'next/link';
+import { handleTextChangeLangWise } from '@/utils/handleLanguage';
+import { LanguageContext } from '@/app/context/language';
 
 function Branches({ branches, serverHost }) {
 
+  const { language } = useContext(LanguageContext);
+
   return (
-    <div className="grid grid-cols-3 my-8 gap-4 ">
-      {branches.map((branch, index) => (
-        <div
-          key={index}
-          className="bg-slate-300 flex flex-col gap-4 mx-auto px-6 py-6 rounded-2xl"
-        >
-            <div className="flex flex-row gap-8 justify-between">
-            <h3 className="font-bold text-black text-xl">{branch.name}</h3>
-            <Link href={branch.map_location} className="text-red-500 text-4xl">
-              <IoLocationSharp />
-            </Link>
-          </div>
-          <div className="flex flex-row gap-4">
-            <div className="flex flex-row gap-1">
-              <p className="bg-green-600 rounded-full my-auto p-1 text-white">
-                <FaPhoneAlt />
-              </p>
-              <p>{branch.phone}</p>
+    <div className=' min-h-screen' >
+      <div className=' text-3xl font-bold py-8 text-center'>{ handleTextChangeLangWise(language, 'ShowIng All Branch','Showing All Branch') }</div>
+      {/* <div className=" flex m-8 flex-wrap bg-sky-500 justify-center align-middle"> */}
+      <div className="flex flex-wrap justify-center object-center gap-4">
+        {branches.map((branch, index) => (
+          <div
+            key={index}
+            className="bg-indigo-50 flex flex-col gap-4 min-w-fit px-6 py-6 rounded-2xl shadow-lg"
+          >
+              <div className="flex flex-row gap-8 justify-between">
+              <h3 className="font-bold text-black text-xl">{branch.name}</h3>
+              <Link href={branch?.map_location || '#'} className="text-red-500 text-4xl" >
+                <IoLocationSharp />
+              </Link>
             </div>
-            {branch.optional_phone && (
+            <div className="flex flex-row gap-4">
               <div className="flex flex-row gap-1">
                 <p className="bg-green-600 rounded-full my-auto p-1 text-white">
                   <FaPhoneAlt />
                 </p>
-                <p>{branch.optional_phone}</p>
+                <p>{branch?.phone}</p>
               </div>
-            )}
-            
+              {branch.optional_phone && (
+                <div className="flex flex-row gap-1">
+                  <p className="bg-green-600 rounded-full my-auto p-1 text-white">
+                    <FaPhoneAlt />
+                  </p>
+                  <p>{branch?.optional_phone}</p>
+                </div>
+              )}
+              
+            </div>
+            <Link href={`http://${branch?.domain || '#'}`} className='flex flex-row'>
+              <p className="text-red-500 mr-1 my-auto font-bold">
+                <FaExternalLinkAlt />
+              </p>
+              <p className="text-red-500 font-semibold">
+                Website
+              </p>
+            </Link>
           </div>
-          <Link href={`http://${branch.domain}`} className='flex flex-row'>
-          <p className="text-red-500 mr-1 my-auto font-bold">
-          <FaExternalLinkAlt />
-
-            </p>
-          <p className="text-red-500 font-semibold">Website
-            </p>
-          </Link>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
