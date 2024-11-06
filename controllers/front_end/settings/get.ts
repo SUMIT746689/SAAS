4178fac332b9499a9b447e526592194f8e5e3bd0
mukta35-2733@diu.edu.bd <mukta35-2733@diu.edu.bd>
2024-11-06 12:39:53 +0600

    settings management added in scholl-sass

--- controllers/front_end/settings/get.ts
+++ controllers/front_end/settings/get.ts
@@ -1,7 +1,6 @@
 import prisma from '@/lib/prisma_client';
 import { authenticate } from 'middleware/authenticate';
 import { logFile } from 'utilities_api/handleLogFile';
-
 async function get(req, res, refresh_token) {
   try {
     const { school_id } = refresh_token;
@@ -10,16 +9,14 @@ async function get(req, res, refresh_token) {
         school_id
       },
       select: {
+        is_branch_wise_fees_collection: true,
         branch_wise_addmission: true,
       }
     });;
-
     res.status(200).json(websiteUirow);
-
   } catch (error) {
     logFile.error(error.message);
     res.status(404).json({ Error: error.message });
   }
 }
-
 export default authenticate(get)
--- controllers/front_end/settings/put.ts
+++ controllers/front_end/settings/put.ts
@@ -5,25 +5,17 @@ async function put(req, res, refresh_token) {
   try {
     const { school_id } = refresh_token;
     const { is_branch_wise_fees_collection, branch_wise_addmission } = req.body;
-    console.log( ".......is_branch_wise_fees_collection",{is_branch_wise_fees_collection})
-    console.log( ".......sjsj",req.body)
+    console.log({ is_branch_wise_fees_collection}, {branch_wise_addmission})
     await prisma.websiteUi.update({
       where: {
         id: school_id
     },
       data: {
         is_branch_wise_fees_collection: typeof is_branch_wise_fees_collection === "boolean" ? is_branch_wise_fees_collection : undefined,
-        // branch_wise_addmission: typeof branch_wise_addmission === "boolean" ? branch_wise_addmission : undefined,
+        branch_wise_addmission: typeof branch_wise_addmission === "boolean" ? branch_wise_addmission : undefined,
       }
   });
   return res.status(200).json({ message: "updated sucessfully" })
-
-//   await prisma.websiteUi.create({
-//     data: {
-//       is_branch_wise_fees_collection: typeof is_branch_wise_fees_collection === "boolean" ? is_branch_wise_fees_collection : undefined,
-//     }
-// });
-// return res.status(200).json({ message: "created sucessfully" })
 
   } catch (error) {
     logFile.error(error.message);
